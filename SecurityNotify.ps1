4f38cceaf70e7b7f8a549e2b377e91269fc79015
gushmazuko <34921843+gushmazuko@users.noreply.github.com>
2018-03-30 00:37:19 +0200

    Security and Maintenance Notifications Control
    Control Security and Maintenance Notifications on Windows 10

---
+++ SecurityNotify.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+	This is script for Control (disable|enable) the Security and Maintenance Notifications on Windows 10
+.NOTES
+	Function   : SecurityNotify
+	File Name  : SecurityNotify.ps1
+	Author     : Gushmazuko
+.LINK
+
+.EXAMPLE
+	powershell.exe IEX(New-Object Net.WebClient).DownloadString('http://192.168.0.30/SecurityNotify.ps1'); SecurityNotify -set 0
+	
+	set 0 = disable & set 1 = enable
+#>
+
+function SecurityNotify(){
+	Param (
+
+		[String]$set
+	)
+
+
+	#Remove registry structure
+	#Remove-Item "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance" -Recurse -Force
+
+	#Create registry structure
+	#Start-Sleep 6
+	New-Item "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance" -Force
+	New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance" -Name "Enabled" -Type "DWORD" -Value $set -Force
+
+	#Check egistry structure
+	#Get-ItemProperty -Path Registry::HKCU\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings\Windows.SystemToast.SecurityAndMaintenance
+}
