abb1e75e1a9395656d5e12745e297ded51b8c02b
gushmazuko <gushmazuko@protonmail.com>
2019-10-06 22:30:13 +0200

    Script update

--- base64_convert.py
+++ base64_convert.py
@@ -8,56 +8,61 @@ from termcolor import colored
 
 
 def banner():
-    print colored("""
+	print colored("""
 ===============================================================================
 
-#####                          #######           #     #                    
+#####                          #######           #     #
 #     #  ####  #####  #####     #     # ######    #     #   ##    ####  #    #
 #       #    # #    # #    #    #     # #         #     #  #  #  #    # #   #
-#       #    # #    # #    #    #     # #####     ####### #    # #      ####  
-#       #    # #####  #####     #     # #         #     # ###### #      #  #  
+#       #    # #    # #    #    #     # #####     ####### #    # #      ####
+#       #    # #####  #####     #     # #         #     # ###### #      #  #
 #     # #    # #   #  #         #     # #         #     # #    # #    # #   #
 #####   ####  #    # #         ####### #         #     # #    #  ####  #    #
-                                                                             
-           ____                             __  __      _          
-          |  _ \ _____      _____ _ __     |  \/  | ___| |_ ___ _ __
-          | |_) / _ \ \ /\ / / _ \ '__|____| |\/| |/ _ \ __/ _ \ '__|
-          |  __/ (_) \ V  V /  __/ | |_____| |  | |  __/ ||  __/ |  
-          |_|   \___/ \_/\_/ \___|_|       |_|  |_|\___|\__\___|_|  
-                                                                   
+
+		   ____                             __  __      _
+		  |  _ \ _____      _____ _ __     |  \/  | ___| |_ ___ _ __
+		  | |_) / _ \ \ /\ / / _ \ '__|____| |\/| |/ _ \ __/ _ \ '__|
+		  |  __/ (_) \ V  V /  __/ | |_____| |  | |  __/ ||  __/ |
+		  |_|   \___/ \_/\_/ \___|_|       |_|  |_|\___|\__\___|_|
 
 
 
-   
-This script  is a PowerShell Command base64 Converter.
+
+
+This script  is a PowerShell Command & Script base64 Converter.
 ===============================================================================
 ""","cyan")
-   
+
 
 def powershell_encode(data):
-    # blank command will store our fixed unicode variable
-    blank_command = ""
-    powershell_command = ""
-    # Remove weird chars that could have been added by ISE
-    n = re.compile(u'(\xef|\xbb|\xbf)')
-    # loop through each character and insert null byte
-    for char in (n.sub("", data)):
-        # insert the nullbyte
-        blank_command += char + "\x00"
-    # assign powershell command as the new one
-    powershell_command = blank_command
-    # base64 encode the powershell command
-    powershell_command = base64.b64encode(powershell_command)
-    return powershell_command
-
+	# blank command will store our fixed unicode variable
+	blank_command = ""
+	powershell_command = ""
+	# Remove weird chars that could have been added by ISE
+	n = re.compile(u'(\xef|\xbb|\xbf)')
+	# loop through each character and insert null byte
+	for char in (n.sub("", data)):
+		# insert the nullbyte
+		blank_command += char + "\x00"
+	# assign powershell command as the new one
+	powershell_command = blank_command
+	# base64 encode the powershell command
+	powershell_command = base64.b64encode(powershell_command)
+	return powershell_command
 
 def metasploit():
-    print colored("[*]PowerShell command convert to base64","yellow")
-    ans_command = raw_input("Write your PowerShell command: ")
-    print colored("\n[*]Наша команда для PowerShell:","yellow")
-    x = powershell_encode(ans_command)
-    print colored("powershell.exe -NoP -NonI -W Hidden -Exec Bypass -enc " + x,"red")
+	print colored("[*]PowerShell command & script convert to base64","yellow")
+	choice = raw_input("1 - Convert a command\n2 - Specify PS1 file to convert\n: ")
+	if choice == "1":
+		ans_command = raw_input("Write your PowerShell command: ")
+		final_command = ans_command
+	elif choice == "2":
+		ans_file = raw_input("Write your PS1 file path: ")
+		file = open(ans_file)
+		final_command = file.read()
+	x = powershell_encode(final_command)
+	print colored("\n[*]Наша команда для PowerShell:","yellow")
+	print colored("powershell.exe -NoP -NonI -W 1 -Exec Bypass -enc " + x,"red")
 
 banner()
 metasploit()
- 
